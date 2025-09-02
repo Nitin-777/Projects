14ccd05d8f60531f020f049b4f356d4b3ed6c84b
Nitin <nitinsharmaop828@gmail.com>
2025-09-02 13:59:02 +0530

    Todo V1

---
+++ To-Do-App/src/App.jsx
@@ -0,0 +1,31 @@
+import AppName from "./components/Appname.jsx";
+import AddTodo from "./components/AddTodo.jsx";
+import TodoItems from "./components/TodoItems.jsx";
+import "./App.css";
+
+function App() {
+  const todoItems = [
+    {
+      name: "Buy Milk",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Go to College",
+      dueDate: "4/10/2023",
+    },
+    {
+      name: "Play Games",
+      dueDate: "right now",
+    },
+  ];
+
+  return (
+    <center className="todo-container">
+      <AppName />
+      <AddTodo />
+      <TodoItems todoItems={todoItems}></TodoItems>
+    </center>
+  );
+}
+
+export default App;
